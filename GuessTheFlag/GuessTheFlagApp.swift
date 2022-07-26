cf2562a529fd16374f6ff22a68030ec03b8b8f86
Eymen <evarilci@gmail.com>
2022-07-26 20:33:36 +0300

    no message

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Eymen Varilci on 26.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
