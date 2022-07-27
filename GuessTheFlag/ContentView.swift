66caae6717a723a5c8cc27c6a3bd938b4f421f86
Eymen <evarilci@gmail.com>
2022-07-27 14:37:42 +0300

    Round indication added and max round fixed to 10. in every 10 rounds it starts over.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -12,6 +12,9 @@ struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var score = 0
+    @State private var gameOver = false
+    @State private var currentRound = 0
+    @State private var maxRound = 9
     
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
@@ -42,7 +45,7 @@ struct ContentView: View {
                     }
                     ForEach(0..<3) {number in
                         Button {
-                            flagTpped(number)
+                            flagTapped(number)
                         } label: {
                             Image(countries[number])
                                 .renderingMode(.original)
@@ -65,6 +68,9 @@ struct ContentView: View {
                 Text("Score: \(score)")
                     .foregroundColor(.white)
                     .font(.title.bold())
+                Text("Current Round is \(currentRound) of \(maxRound + 1)")
+                    .foregroundColor(.white)
+                    .font(.body.bold())
                 Spacer()
                 
                 
@@ -72,19 +78,44 @@ struct ContentView: View {
             }
             .padding()
         }
-        .alert(scoreTitle, isPresented: $showingScore) {
-            Button("continue", action: askQuestion)
-        } message: {
-            Text("Your Score is \(score)")
-        }
+        .alert(isPresented: $showingScore) {
+                    if gameOver {
+                        return Alert(title: Text("Game over"),
+                              message:Text("Your score is \(score)"),
+                              dismissButton: .default(Text("Restart")){
+                                self.reset() // reset game here
+                            })
+                    } else {
+                        return Alert(title: Text(scoreTitle),
+                              message:Text("Your score is \(score)"),
+                              dismissButton: .default(Text("Continue")){
+                                self.askQuestion()
+                            })
+                    }
+                }
     }
     
-    func flagTpped(_ number: Int) {
+    func reset() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        score = 0
+        currentRound = 0
+    }
+    
+    func flagTapped(_ number: Int){
         if number == correctAnswer {
-            scoreTitle = "Correct"
             score += 1
+            scoreTitle = "Correct"
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong! That's the flag of \(number)"
+        }
+            
+        gameOver = currentRound == maxRound
+        // score = 0        // reset this at the "Restart" call
+        // currentRound = 0 // reset this at the "Restart" call
+        
+        if currentRound < maxRound {
+            currentRound += 1
         }
         showingScore = true
     }
